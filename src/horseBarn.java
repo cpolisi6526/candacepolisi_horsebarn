f21c34cf5cc5f2c0c103528fbf1708a94e658f1d
canda <cpolisi6526@bths.edu>
2018-11-26 19:35:22 -0500

    redo

--- src/horseBarn.java
+++ src/horseBarn.java
@@ -1,25 +1,28 @@
 public class horseBarn {
+    // the field is the horses in the array called spaces.
     private horse[] spaces;
     public horseBarn(horse[] spaces){
-        this.spaces=spaces;
+        this.spaces = spaces;
     }
+
     public int findHorseSpace(String name){
-    for (int i= 0; i<this.spaces.length; i++){
-        if ((this.spaces[i]!=null) && (name.equals(this.spaces[i].getName()))){
-            return i;
-        }
-    }
-    return -1;
-    }
-    public void consolidate(){
-        for (int i=0; i<this.spaces.length-1;i++){
-            if (this.spaces[i] == null){
-                for (int j = i+1; j<this.spaces.length; j++){
-                    this.spaces[i]= this.spaces[j];
-                    this.spaces[j]= null;
-                    j= this.spaces.length;
-                }
+        for(int i=0; i<spaces.length; i++){
+            if (spaces[i] != null && name.equals(spaces[i].getName())){
+                return i;
             }
         }
+        return -1;
+    }
+
+    public void consolidate(){
+        horse[] newSpaces = new horse[spaces.length];
+        int newPlaces = 0;
+        for(int i = 0; i<spaces.length; i++){
+            if (spaces[i] != null){
+                newSpaces[newPlaces] = spaces[i];
+                newPlaces++;
+            }
+        }
+        spaces = newSpaces;
     }
 }
--- src/runner.java
+++ src/runner.java
@@ -1,24 +1,27 @@
-public class HorseRunner {
-    public static void main(String[] args){
-        horse horse1= new mustang("Secretariat", 12000);
-        horse horse2= new mustang("Dusty Trail", 22000);
-        horse horse3= new mustang("Silver",12000);
+public class runner {
 
-        horse[] horses= new horse[6];
+    public static void main(String[] args) {
+        horse horse1 = new mustang("whale",52);
+        horse horse2 = new mustang("bug", 53);
+        horse horse3 = new mustang("horsey",54);
+        horse[] spot = {horse1, horse2, horse3};
+        for(int i=0; i<spot.length;i++){
+            System.out.println(spot[i].toString());
+        }
 
-        horses[1]=horse1;
-        horses[3]=horse2;
-        horses[5]=horse3;
-        horseBarn barn= new horseBarn(horses);
+        horse[] horses = new horse[6];
+        horses[1] = horse1;
+        horses[3] = horse2;
+        horses[5] = horse3;
+        horseBarn barn = new horseBarn(horses);
 
         System.out.println(barn);
-        System.out.println("Dusty Trail is in space: " + barn.findHorseSpace("Dusty Trail"));
+        System.out.println("Whale is in space: "+ barn.findHorseSpace("whale"));
 
         barn.consolidate();
 
         System.out.println(barn);
-        System.out.println("Dusty trail is now in space: " + barn.findHorseSpace("Dusty trail"));
+        System.out.println("Whale is now in space: " + barn.findHorseSpace("whale"));
 
-        }
-
+    }
 }
